c8fea93eb98c2717a55ff08f58ed54cef6c343cf
Guillermo <grobles64@ieszaidinvergeles.org>
2025-02-20 12:14:28 +0100

    Carpeta screens y carpeta widgets

--- lib/screens/receta_diaria_screen.dart
+++ lib/screens/receta_diaria_screen.dart
@@ -42,23 +42,26 @@ class _RecetaDiariaScreenState extends State<RecetaDiariaScreen> {
             ),
             Padding(
               padding: const EdgeInsets.all(20.0),
-              child: Container(
-                color: const Color(0xFF00B894).withAlpha(50),
-                child: ListView(
-                  physics: const NeverScrollableScrollPhysics(),
-                  shrinkWrap: true,
-                  children: _ingredientes.map((ingrediente) {
-                    return CheckboxListTile(
-                      activeColor: const Color(0xFF00B894),
-                      title: Text(ingrediente["nombre"] as String),
-                      value: ingrediente["seleccionado"],
-                      onChanged: (bool? nuevoValor) {
-                        setState(() {
-                          ingrediente["seleccionado"] = nuevoValor!;
-                        });
-                      },
-                    );
-                  }).toList(),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(25),
+                child: Container(
+                  color: const Color(0xFF00B894).withAlpha(50),
+                  child: ListView(
+                    physics: const NeverScrollableScrollPhysics(),
+                    shrinkWrap: true,
+                    children: _ingredientes.map((ingrediente) {
+                      return CheckboxListTile(
+                        activeColor: const Color(0xFF00B894),
+                        title: Text(ingrediente["nombre"] as String),
+                        value: ingrediente["seleccionado"],
+                        onChanged: (bool? nuevoValor) {
+                          setState(() {
+                            ingrediente["seleccionado"] = nuevoValor!;
+                          });
+                        },
+                      );
+                    }).toList(),
+                  ),
                 ),
               ),
             ),
@@ -89,7 +92,7 @@ class _RecipePreparation extends StatelessWidget {
           color: const Color(0xFF00B894).withAlpha(50),
           padding: const EdgeInsets.all(20),
           child: const Text(
-              'Irure quis ut quis occaecat qui do nulla consequat nostrud. Anim non occaecat velit nostrud cupidatat laborum aliquip velit mollit officia aliquip dolore. Est aliqua eu sint laborum quis esse. Dolore qui minim amet eu occaecat aliqua dolor labore est quis nisi incididunt occaecat. Ipsum mollit occaecat exercitation proident. Ea ex ullamco dolore sunt consectetur sunt duis dolor elit incididunt dolor magna. Deserunt magna tempor aliqua adipisicing officia ex mollit. Veniam proident ut nisi proident ex eiusmod sunt incididunt mollit reprehenderit. Nulla officia consequat fugiat voluptate voluptate anim magna nisi officia dolor veniam. Duis quis incididunt Lorem in. Commodo eiusmod dolore minim dolore culpa reprehenderit pariatur cillum. Laboris anim minim velit laborum dolor in consequat culpa. Occaecat ad sint magna sit. Et mollit esse sint do cillum eiusmod commodo tempor amet pariatur sint eu pariatur veniam. Nostrud elit cupidatat id sit anim laboris velit nostrud. Dolore laboris cillum commodo consequat incididunt nostrud amet elit amet ipsum fugiat ut. Irure minim et commodo sunt nisi nostrud esse non ut ea fugiat incididunt ipsum. Cupidatat laborum cillum cupidatat aute nisi exercitation esse ex aliquip aliquip magna id. Duis velit aliquip duis cillum. Consectetur ut reprehenderit sit culpa deserunt ea excepteur proident labore voluptate dolor ullamco cillum.'),
+              'Iruraborum aliquip velit mollit officia aliquip dolore. Est aliqua eu sint laborum quis esse. Dolore qui minim amet eu occaecat aliqua dolor labore est quis nisi incididunt occaecat. Ipsum mollit occaecat exercitation proident. Ea ex ullamco dolore sunt consectetur sunt duis dolor elit incididunt dolor magna. Deserunt magna tempor aliqua adipisicing officia ex mollit. Veniam proident ut nisi proident ex eiusmod sunt incididunt mollit reprehenderit. Nulla officia consequat fugiat voluptate voluptate anim magna nisi officia dolor veniam. int magna sit. Et mollit esse sint do cillum eiusmod commodo tempor amet pariatur sint eu pariatur veniam. Nostrud elit cupidatat id sit anim laboris velit nostrud. Dolore laboris cillum commodo consequat incididunt nostrud amet elit amet ipsum fugiat ut. Irure minim et commodo sunt nisi nostrud esse non ut ea fugiat incididunt ipsum. Cupidatat laborum cillum cupidatat aute nisi exercitation esse ex aliquip aliquip magna id. Duis velit aliquip duis cillum. Consectetur ut reprehenderit sit culpa deserunt ea excepteur proident labore voluptate dolor ullamco cillum.'),
         ),
       ),
     );
