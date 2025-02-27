4e562183695afdf1ae08d14d7a013699b2a70ff9
Guillermo <grobles64@ieszaidinvergeles.org>
2025-02-27 10:17:38 +0100

    Sesión 6. Refactor SwiperFood

--- lib/widgets/SwiperFood.dart
+++ lib/widgets/SwiperFood.dart
@@ -1,5 +1,6 @@
 import 'package:card_swiper/card_swiper.dart';
 import 'package:fl_nutrimate/entities/entities.dart';
+import 'package:fl_nutrimate/screens/cambio_receta_screen.dart';
 import 'package:fl_nutrimate/screens/receta_diaria_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -25,14 +26,8 @@ class _SwiperFoodState extends State<SwiperFood> {
       children: [
         Padding(
           padding: const EdgeInsets.only(top: 16.0),
-          child: Text(
-            widget.recipes[currentIndex].type.name.toUpperCase(),
-            style: const TextStyle(
-              fontSize: 18,
-              fontWeight: FontWeight.bold,
-              color: Color(0xFF00B894),
-            ),
-          ),
+          child:
+              _SwiperTimeFoodLabel(widget: widget, currentIndex: currentIndex),
         ),
         Padding(
           padding: const EdgeInsets.all(20.0),
@@ -46,44 +41,9 @@ class _SwiperFoodState extends State<SwiperFood> {
                 });
               },
               itemBuilder: (BuildContext context, int index) {
-                return ClipRRect(
-                  borderRadius: BorderRadius.circular(25),
-                  child: Stack(
-                    fit: StackFit.expand,
-                    children: [
-                      Positioned.fill(
-                        child: GestureDetector(
-                          onTap: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => RecetaDiariaScreen(
-                                    receta: widget.recipes[index]),
-                              ),
-                            );
-                          },
-                          child: FadeInImage(
-                            placeholder: const AssetImage('assets/loading.gif'),
-                            image: NetworkImage(widget.recipes[index].imageUrl),
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                      ),
-                      Positioned(
-                        top: 8,
-                        right: 8,
-                        child: CircleAvatar(
-                          backgroundColor: Colors.black54,
-                          radius: 18,
-                          child: IconButton(
-                            icon: Icon(widget.icon,
-                                color: Colors.white, size: 18),
-                            onPressed: () {},
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
+                return _SwiperImage(
+                  widget: widget,
+                  receta: widget.recipes[index],
                 );
               },
               pagination: SwiperPagination(
@@ -101,6 +61,102 @@ class _SwiperFoodState extends State<SwiperFood> {
         ),
         const Divider(),
       ],
+    );
+  }
+}
+
+class _SwiperTimeFoodLabel extends StatelessWidget {
+  const _SwiperTimeFoodLabel({
+    super.key,
+    required this.widget,
+    required this.currentIndex,
+  });
+
+  final SwiperFood widget;
+  final int currentIndex;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      widget.recipes[currentIndex].type.name.toUpperCase(),
+      style: const TextStyle(
+        fontSize: 18,
+        fontWeight: FontWeight.bold,
+        color: Color(0xFF00B894),
+      ),
+    );
+  }
+}
+
+class _SwiperImage extends StatelessWidget {
+  final Recipe receta;
+  const _SwiperImage({
+    super.key,
+    required this.widget,
+    required this.receta,
+  });
+
+  final SwiperFood widget;
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(25),
+      child: Stack(
+        fit: StackFit.expand,
+        children: [
+          Positioned.fill(
+            child: GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => RecetaDiariaScreen(receta: receta),
+                  ),
+                );
+              },
+              child: FadeInImage(
+                placeholder: const AssetImage('assets/loading.gif'),
+                image: NetworkImage(receta.imageUrl),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          _SwiperIcon(widget: widget),
+        ],
+      ),
+    );
+  }
+}
+
+class _SwiperIcon extends StatelessWidget {
+  const _SwiperIcon({
+    super.key,
+    required this.widget,
+  });
+
+  final SwiperFood widget;
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      top: 8,
+      right: 8,
+      child: CircleAvatar(
+        backgroundColor: Colors.black54,
+        radius: 18,
+        child: IconButton(
+          icon: Icon(widget.icon, color: Colors.white, size: 18),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const CambioRecetaScreen(),
+              ),
+            );
+          },
+        ),
+      ),
     );
   }
 }
